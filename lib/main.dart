1aa560e778c565c2506cf879d9c3e526c7d7a88b
alxayeed <alxayeed@gmail.com>
2022-12-11 17:19:28 +0600

    adding data to firestore

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -10,58 +14,66 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Firebase Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.orange,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  TextEditingController controller = TextEditingController();
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  // Future fetchPlayers() async {
+  //   var firestore = FirebaseFirestore.instance;
+  //   QuerySnapshot querySnapshot = await firestore.collection("Players").get();
+  //   return querySnapshot.docs;
+  // }
+
+  Future addPlayer({required String name}) async {
+    final docUser =
+        FirebaseFirestore.instance.collection("players").doc('doc-id2');
+
+    final player = {'name': name, 'age': "25", 'country': 'N/A', "club": ""};
+
+    docUser.set(player);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        appBar: AppBar(
+          title: const Text('Firebase Demo'),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
+        body: Container(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(18.0),
+                child: TextField(
+                  decoration: const InputDecoration(hintText: 'Name'),
+                  controller: controller,
+                ),
+              ),
+              TextButton(
+                  onPressed: () {
+                    final name = controller.text;
+
+                    addPlayer(name: name);
+                  },
+                  child: const Text('Save', style: TextStyle(fontSize: 18)))
+            ],
+          ),
+        ));
   }
 }
